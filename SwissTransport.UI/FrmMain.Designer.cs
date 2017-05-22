90976f1edc5a50a4b0947a64304535c77459f51d
BBZW-Sursee Thuering Lukas <Lukas_Thuering@sluz.ch>
2017-05-22 13:27:18 +0200

    A001 - A003, A006. Commented everything

--- SwissTransport.UI/FrmMain.Designer.cs
+++ SwissTransport.UI/FrmMain.Designer.cs
@@ -36,14 +36,17 @@ namespace SwissTransport
             this.DateTimeDate = new System.Windows.Forms.DateTimePicker();
             this.BtnConnections = new System.Windows.Forms.Button();
             this.DateTimeClock = new System.Windows.Forms.DateTimePicker();
-            this.btnswap = new System.Windows.Forms.Button();
+            this.BtnSwap = new System.Windows.Forms.Button();
             this.transportBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.ListViewResult = new System.Windows.Forms.ListView();
-            this.clmtrack = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmto = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.clmdeparture = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmOperator = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.clmarrival = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.clmduration = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.btnTimeable = new System.Windows.Forms.Button();
+            this.clmtrack = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.BtnTimetable = new System.Windows.Forms.Button();
+            this.BtnGmapsStart = new System.Windows.Forms.Button();
+            this.BtnGmapsEnd = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.transportBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,11 +55,12 @@ namespace SwissTransport
             this.ComboBoxStart.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
             this.ComboBoxStart.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
             this.ComboBoxStart.FormattingEnabled = true;
-            this.ComboBoxStart.Location = new System.Drawing.Point(68, 87);
+            this.ComboBoxStart.Location = new System.Drawing.Point(12, 88);
             this.ComboBoxStart.Name = "ComboBoxStart";
             this.ComboBoxStart.Size = new System.Drawing.Size(243, 24);
             this.ComboBoxStart.TabIndex = 0;
             this.ComboBoxStart.DropDown += new System.EventHandler(this.ComboBoxStartStop_DropDown);
+            this.ComboBoxStart.TextChanged += new System.EventHandler(this.ComboBoxStart_TextChanged);
             // 
             // ComboBoxEnd
             // 
@@ -66,16 +70,18 @@ namespace SwissTransport
             this.ComboBoxEnd.Size = new System.Drawing.Size(243, 24);
             this.ComboBoxEnd.TabIndex = 1;
             this.ComboBoxEnd.DropDown += new System.EventHandler(this.ComboBoxStartStop_DropDown);
+            this.ComboBoxEnd.TextChanged += new System.EventHandler(this.ComboBoxEnd_TextChanged);
             // 
             // DateTimeDate
             // 
-            this.DateTimeDate.Location = new System.Drawing.Point(647, 91);
+            this.DateTimeDate.Location = new System.Drawing.Point(725, 91);
             this.DateTimeDate.Name = "DateTimeDate";
             this.DateTimeDate.Size = new System.Drawing.Size(163, 22);
             this.DateTimeDate.TabIndex = 2;
             // 
             // BtnConnections
             // 
+            this.BtnConnections.Enabled = false;
             this.BtnConnections.Location = new System.Drawing.Point(894, 171);
             this.BtnConnections.Name = "BtnConnections";
             this.BtnConnections.Size = new System.Drawing.Size(157, 43);
@@ -87,20 +93,21 @@ namespace SwissTransport
             // DateTimeClock
             // 
             this.DateTimeClock.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.DateTimeClock.Location = new System.Drawing.Point(816, 91);
+            this.DateTimeClock.Location = new System.Drawing.Point(894, 91);
             this.DateTimeClock.Name = "DateTimeClock";
             this.DateTimeClock.ShowUpDown = true;
             this.DateTimeClock.Size = new System.Drawing.Size(112, 22);
             this.DateTimeClock.TabIndex = 4;
             // 
-            // btnswap
+            // BtnSwap
             // 
-            this.btnswap.Location = new System.Drawing.Point(317, 74);
-            this.btnswap.Name = "btnswap";
-            this.btnswap.Size = new System.Drawing.Size(75, 56);
-            this.btnswap.TabIndex = 6;
-            this.btnswap.Text = "----->\r\n<-----\r\n";
-            this.btnswap.UseVisualStyleBackColor = true;
+            this.BtnSwap.Location = new System.Drawing.Point(317, 74);
+            this.BtnSwap.Name = "BtnSwap";
+            this.BtnSwap.Size = new System.Drawing.Size(75, 56);
+            this.BtnSwap.TabIndex = 6;
+            this.BtnSwap.Text = "----->\r\n<-----\r\n";
+            this.BtnSwap.UseVisualStyleBackColor = true;
+            this.BtnSwap.Click += new System.EventHandler(this.btnswap_Click);
             // 
             // transportBindingSource
             // 
@@ -109,10 +116,11 @@ namespace SwissTransport
             // ListViewResult
             // 
             this.ListViewResult.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.clmtrack,
+            this.clmto,
             this.clmdeparture,
+            this.clmOperator,
             this.clmarrival,
-            this.clmduration});
+            this.clmtrack});
             this.ListViewResult.Location = new System.Drawing.Point(68, 171);
             this.ListViewResult.Name = "ListViewResult";
             this.ListViewResult.Size = new System.Drawing.Size(809, 348);
@@ -120,36 +128,60 @@ namespace SwissTransport
             this.ListViewResult.UseCompatibleStateImageBehavior = false;
             this.ListViewResult.View = System.Windows.Forms.View.Details;
             // 
-            // clmtrack
+            // clmto
             // 
-            this.clmtrack.Text = "Gleis";
+            this.clmto.Text = "Nach";
+            this.clmto.Width = 107;
             // 
             // clmdeparture
             // 
             this.clmdeparture.Text = "Abfahrt";
             this.clmdeparture.Width = 207;
             // 
+            // clmOperator
+            // 
+            this.clmOperator.Text = "Betreiber";
+            this.clmOperator.Width = 126;
+            // 
             // clmarrival
             // 
-            this.clmarrival.DisplayIndex = 3;
             this.clmarrival.Text = "Ankunft";
             this.clmarrival.Width = 253;
             // 
-            // clmduration
+            // clmtrack
             // 
-            this.clmduration.DisplayIndex = 2;
-            this.clmduration.Text = "Reisedauer";
-            this.clmduration.Width = 202;
+            this.clmtrack.Text = "Gleis";
             // 
-            // btnTimeable
+            // BtnTimetable
             // 
-            this.btnTimeable.Location = new System.Drawing.Point(894, 220);
-            this.btnTimeable.Name = "btnTimeable";
-            this.btnTimeable.Size = new System.Drawing.Size(157, 43);
-            this.btnTimeable.TabIndex = 8;
-            this.btnTimeable.Text = "Fahrplan anzeigen";
-            this.btnTimeable.UseVisualStyleBackColor = true;
-            this.btnTimeable.Click += new System.EventHandler(this.btnTimeable_Click);
+            this.BtnTimetable.Enabled = false;
+            this.BtnTimetable.Location = new System.Drawing.Point(894, 220);
+            this.BtnTimetable.Name = "BtnTimetable";
+            this.BtnTimetable.Size = new System.Drawing.Size(157, 43);
+            this.BtnTimetable.TabIndex = 8;
+            this.BtnTimetable.Text = "Fahrplan anzeigen";
+            this.BtnTimetable.UseVisualStyleBackColor = true;
+            this.BtnTimetable.Click += new System.EventHandler(this.btnTimetable_Click);
+            // 
+            // BtnGmapsStart
+            // 
+            this.BtnGmapsStart.Location = new System.Drawing.Point(261, 89);
+            this.BtnGmapsStart.Name = "BtnGmapsStart";
+            this.BtnGmapsStart.Size = new System.Drawing.Size(50, 23);
+            this.BtnGmapsStart.TabIndex = 9;
+            this.BtnGmapsStart.Text = "Maps";
+            this.BtnGmapsStart.UseVisualStyleBackColor = true;
+            this.BtnGmapsStart.Click += new System.EventHandler(this.BtnGmapsStart_Click);
+            // 
+            // BtnGmapsEnd
+            // 
+            this.BtnGmapsEnd.Location = new System.Drawing.Point(647, 90);
+            this.BtnGmapsEnd.Name = "BtnGmapsEnd";
+            this.BtnGmapsEnd.Size = new System.Drawing.Size(50, 23);
+            this.BtnGmapsEnd.TabIndex = 10;
+            this.BtnGmapsEnd.Text = "Maps";
+            this.BtnGmapsEnd.UseVisualStyleBackColor = true;
+            this.BtnGmapsEnd.Click += new System.EventHandler(this.BtnGmapsEnd_Click);
             // 
             // FrmMain
             // 
@@ -157,9 +189,11 @@ namespace SwissTransport
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1063, 531);
-            this.Controls.Add(this.btnTimeable);
+            this.Controls.Add(this.BtnGmapsEnd);
+            this.Controls.Add(this.BtnGmapsStart);
+            this.Controls.Add(this.BtnTimetable);
             this.Controls.Add(this.ListViewResult);
-            this.Controls.Add(this.btnswap);
+            this.Controls.Add(this.BtnSwap);
             this.Controls.Add(this.DateTimeClock);
             this.Controls.Add(this.BtnConnections);
             this.Controls.Add(this.DateTimeDate);
@@ -179,13 +213,16 @@ namespace SwissTransport
         private System.Windows.Forms.DateTimePicker DateTimeDate;
         private System.Windows.Forms.Button BtnConnections;
         private System.Windows.Forms.DateTimePicker DateTimeClock;
-        private Button btnswap;
+        private Button BtnSwap;
         private BindingSource transportBindingSource;
         private ListView ListViewResult;
-        private Button btnTimeable;
+        private Button BtnTimetable;
         private ColumnHeader clmtrack;
         private ColumnHeader clmdeparture;
-        private ColumnHeader clmduration;
         private ColumnHeader clmarrival;
+        private ColumnHeader clmto;
+        private ColumnHeader clmOperator;
+        private Button BtnGmapsStart;
+        private Button BtnGmapsEnd;
     }
 }
--- SwissTransport.UI/FrmMain.cs
+++ SwissTransport.UI/FrmMain.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,70 +13,144 @@ namespace SwissTransport
 {
     public partial class FrmMain : Form
     {
-        Transport mainTransport;
+        private readonly ITransport mainTransport = new Transport();
+
         public FrmMain()
         {
             InitializeComponent();
-
         }
+        
 
-        private void BtnConnections_Click(object sender, EventArgs e)
+    //This event controls the actions, if the Button "Verbindung suchen" is clicked.
+    private void BtnConnections_Click(object sender, EventArgs e)
         {
+            //The items and columns in the ListView will be cleared if the button is clicked. Also 4 new Colums are getting created.
+            //The Event .Items.Clear and .Colums.Clear, deletes eery item and every Column in the ListView.
+            //.Columns.Add ist responsible for creating new Colums. These colums are used to view the Details for the Route.
+            ListViewResult.Items.Clear();
+            ListViewResult.Columns.Clear();
+            ListViewResult.Columns.Add("Gleis");
+            ListViewResult.Columns.Add("Abfahrt");
+            ListViewResult.Columns.Add("Dauer");
+            ListViewResult.Columns.Add("Ankunft");
 
-            
-                
-            
-            //SwissTransport.Transport transport = new SwissTransport.Transport();
+            //In this section of the Code the Connections are geting calculated.
+            //The object "transport" ist geting created from the class "SwissTransoport.Transport"
+            //The variable "foundConnections" is geting defined. Now the Values from the two Comboboxes are written to the variable.
+            //The ListView gets again cleared from the Items. If the user wants to search for another connection between two new Stations,
+            //The Items will be cleared if the user clicks the button again.
+            //Now the foreach loop calculates the connections between the two stations, which are selected by the user.
+
+            SwissTransport.Transport transport = new SwissTransport.Transport();
             var foundConnections = transport.GetConnections(ComboBoxStart.Text, ComboBoxEnd.Text);
             ListViewResult.Items.Clear();
             foreach (Connection conn in transport.GetConnections(ComboBoxStart.Text, ComboBoxEnd.Text).ConnectionList)
             {
+                //The Date and the Time is geting parsed from the Depature and Arrival time.
+                //After this step, the string for the ListView is getting created. the Values for the connection are saved in the string now.
+                //Then, the String is added to the Listview with the .Items.Add(lvitem)
+                //In the end the Colums are getting auto resized, for a better view.
                 DateTime departure = DateTime.Parse(conn.From.Departure);
                 DateTime arrival = DateTime.Parse(conn.To.Arrival);
                 String[] item = { conn.From.Platform, departure.ToString(), conn.Duration, arrival.ToString()};
                 ListViewItem lvitem = new ListViewItem(item);
                 ListViewResult.Items.Add(lvitem);
+                ListViewResult.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                ListViewResult.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
             }
         }
 
-      
+        //In this code section the Stations will be written down to the Comboboxes.
         private void ComboBoxStartStop_DropDown(object sender, EventArgs e)
         {
+            //The stations from the API are written down to the checkbox
             ComboBox comboboxload = (ComboBox)sender;
             SwissTransport.Transport transport = new SwissTransport.Transport();
             var foundStations = transport.GetStations(comboboxload.Text);
 
-            foreach(var station in foundStations.StationList)
+            //Every station, that is found, is written down in the che^ckbox.
+            foreach (var station in foundStations.StationList)
             {
                 comboboxload.Items.Add(station.Name);
             }
             
         }
-
-        private void btnTimeable_Click(object sender, EventArgs e)
+        //With this Button, the Connections from one Station will be showed to the user.
+        private void btnTimetable_Click(object sender, EventArgs e)
         {
+            //Again the items and de colums are getting cleared, and new Columns are getting created.
             ListViewResult.Items.Clear();
-            string Abfahrt = ComboBoxStart.SelectedItem.ToString();
-            string id = mainTransport.GetStations(Abfahrt).StationList[0].Id;
-            StationBoardRoot stationboardroot = mainTransport.GetStationBoard(Abfahrt, id);
-            foreach (StationBoardRoot stationboard in stationboardroot.Entries);
+            ListViewResult.Columns.Clear();
+            ListViewResult.Columns.Add("Verkehrsmittel");
+            ListViewResult.Columns.Add("Nach");
+            ListViewResult.Columns.Add("Abfahrt");
+            ListViewResult.Columns.Add("Betreiber");
+            
+
+            //In this section the Departures from one selected station will be calculated.
+            string strAbfahrt = ComboBoxStart.SelectedItem.ToString();
+            string id = mainTransport.GetStations(strAbfahrt).StationList[0].Id;
+            StationBoardRoot sbRoot = mainTransport.GetStationBoard(strAbfahrt, id);
+            //In this loop, the Results for the Departures are getting calculated, and the Items will be wirtten down,
+            //to a string, then, a new ListViewItem is created and will be written down in the ListView.
+            foreach ( StationBoard sb in sbRoot.Entries)
             {
-                //foreach machen micha bild
+                String[] result = { sb.Name, sb.To, sb.Stop.Departure.ToString(), sb.Operator};
+                ListViewItem lvItem = new ListViewItem(result);
+                ListViewResult.Items.Add(lvItem);
             }
+            //In the end the Colums are getting auto resized again, for a better view.
+            ListViewResult.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            ListViewResult.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
 
+        }
+        //This Button is responsible to switch the selected Deparute and Arrival Station.
+        private void btnswap_Click(object sender, EventArgs e)
+        {
+            //The String from the ComboBoxStart is now the string "from"
+            string from = "";
 
+            
+            from = ComboBoxStart.Text;
 
-            StationBoardRoot. = new SwissTransport.StationBoard();
-            var foundDepartures = StationBoard.GetStationBoard(ComboBoxStart.Text);
-            ListViewResult.Items.Clear();
-            foreach (StationBoard conn in transport.GetStationBoard(ComboBoxStart.Text).ConnectionList)
+            //Now the new created string "from" can easily be switched with the existing Text from the ComboBoxEnd.
+            ComboBoxStart.Text = ComboBoxEnd.Text;
+            ComboBoxEnd.Text = from;
+            return;
+        }
+        //This Event tests if there is Text in the Combobox, if theres Text in the ComboBoxStart, the Connections Button will be disabled,
+        //and the Timetable Button ist getting enabled.
+        private void ComboBoxStart_TextChanged(object sender, EventArgs e)
+        {
+            
+            
+            if(ComboBoxStart.Text != "")
             {
-                DateTime departure = DateTime.Parse(conn.From.Departure);
-                DateTime arrival = DateTime.Parse(conn.To.Arrival);
-                String[] item = { conn.From.Platform, departure.ToString(), conn.Duration, arrival.ToString() };
-                ListViewItem lvitem = new ListViewItem(item);
-                ListViewResult.Items.Add(lvitem);
+                BtnConnections.Enabled = false;
+                BtnTimetable.Enabled = true;
             }
+            
+        }
+        //This Event tests if there is Text in the Combobox, if theres Text in the ComboBoxStart, the Connections Button will be enabled,
+        //and the Timetable Button ist getting disabled.
+        private void ComboBoxEnd_TextChanged(object sender, EventArgs e)
+        {
+            if (ComboBoxStart.Text != "")
+            {
+                BtnConnections.Enabled = true;
+                BtnTimetable.Enabled = false;
+            }
+        }
+        //Here is the Google Maps intergration, it open the Googe Maps URL and attaches the Text from th ComboBox and the additional Text Haltestelle.
+        private void BtnGmapsStart_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.google.com/maps/place/" + ComboBoxStart.Text + " Haltestelle");
+        }
+
+        private void BtnGmapsEnd_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.google.com/maps/place/" + ComboBoxEnd.Text + " Haltestelle");
         }
     }
 }
+
